29d2f54bd2909a022d028607bd9760572a7279ab
pardo312 <daniel.pardo@timba.co>
2022-10-16 18:51:14 -0500

    Enemy zones

---
+++ GJP2022/Assets/Scripts/EnemyModule/EnemyZoneParams.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyZones", menuName = "GJP2022/EnemyZones", order = 0)]
+public class EnemyZoneParams : ScriptableObject
+{
+    public int enemyRangeSpawn;
+    public int enemyMeleeSpawn;
+    public List<Vector3> spawnPoints;
+}
